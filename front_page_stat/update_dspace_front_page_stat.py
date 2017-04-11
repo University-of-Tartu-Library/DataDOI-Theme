b751c0638db5cfdd883adece214b87aeebc8ffb2
Maksim Misin <mishin1991@gmail.com>
2017-04-11 11:04:01 +0300

    compatability with kast changes

--- front_page_stat/update_dspace_front_page_stat.py
+++ front_page_stat/update_dspace_front_page_stat.py
@@ -3,7 +3,13 @@ import requests
 import sys
 from xml.etree import ElementTree as ET
 
-solr_query_string = "http://localhost:80/solr/statistics/select?q=type:2&fq=statistics_type:view&fq=-isBot:true&fq=time:[NOW-7DAY%20TO%20NOW]&fl=uid+score+&df=id&indent=true&facet=true&facet.field=id&omitHeader=true&f.id.facet.limit=5"
+# get port
+if len(sys.argv) > 2:
+    port = sys.argv[2]
+else:
+    port = 80
+
+solr_query_string = "http://localhost:{}/solr/statistics/select?q=type:2&fq=statistics_type:view&fq=-isBot:true&fq=time:[NOW-7DAY%20TO%20NOW]&fl=uid+score+&df=id&indent=true&facet=true&facet.field=id&omitHeader=true&f.id.facet.limit=5".format(port)
 
 # try getting last 7 days most accessed items
 try:
@@ -15,7 +21,7 @@ except requests.exceptions.RequestException as e:  # This is the correct syntax
 
 def uid_to_handle(uid):
 	""" query solr to get item handle"""
-	query  = "http://localhost:80/solr/search/select?q=search.resourcetype:2+AND+search.resourceid:{}&fl=handle&omitHeader=true&json=true"
+	query  = "http://localhost:{}/solr/search/select?q=search.resourcetype:2+AND+search.resourceid:{}&fl=handle&omitHeader=true&json=true".format(port)
 	resp = requests.get(query.format(uid))
 	tree = ET.fromstring(resp.content)
 	try:
