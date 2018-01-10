173fe0ac346adfa08fd7b757c6dd9b89cfd1c2d3
root <root@datadoi.ut.ee>
2018-01-10 10:34:00 +0200

    update works even if solr outputs a few wrong uuids

--- front_page_stat/update_dspace_front_page_stat.py
+++ front_page_stat/update_dspace_front_page_stat.py
@@ -3,13 +3,7 @@ import requests
 import sys
 from xml.etree import ElementTree as ET
 
-# get port
-if len(sys.argv) > 2:
-    port = sys.argv[2]
-else:
-    port = 80
-
-solr_query_string = "http://localhost:{}/solr/statistics/select?q=type:2&fq=statistics_type:view&fq=-isBot:true&fq=time:[NOW-7DAY%20TO%20NOW]&fl=uid+score+&df=id&indent=true&facet=true&facet.field=id&omitHeader=true&f.id.facet.limit=5".format(port)
+solr_query_string = "http://localhost:80/solr/statistics/select?q=type:2&fq=statistics_type:view&fq=-isBot:true&fq=time:[NOW-7DAY%20TO%20NOW]&fl=uid+score+&df=id&indent=true&facet=true&facet.field=id&omitHeader=true&f.id.facet.limit=50"
 
 # try getting last 7 days most accessed items
 try:
@@ -21,23 +15,31 @@ except requests.exceptions.RequestException as e:  # This is the correct syntax
 
 def uid_to_handle(uid):
 	""" query solr to get item handle"""
-	query  = "http://localhost:{}/solr/search/select?q=search.resourcetype:2+AND+search.resourceid:{}&fl=handle&omitHeader=true&json=true".format(port)
+	query  = "http://localhost:80/solr/search/select?q=search.resourcetype:2+AND+search.resourceid:{}&fl=handle&omitHeader=true&json=true"
 	resp = requests.get(query.format(uid))
 	tree = ET.fromstring(resp.content)
 	try:
 		return tree.find(".//str").text
 	except AttributeError:
-		print(ET.dump(tree))
-		sys.exit(1)
+                print('Couldnt find corresponding handle for uid={}!'.format(uid))
+		#print(ET.dump(tree))
+                return None
+		
 
 # parse tree and find relevant items
 tree = ET.fromstring(resp.content)
 
-popular_items = []
+popular_items = []  # contains handles and number of views
 for item in tree.iter("int"):
 	uid = item.get("name")
+        handle = uid_to_handle(uid)
 	accessed = item.text
-	popular_items.append((uid, accessed))
+        if handle:
+            print('{} corresponds to {}'.format(uid, handle))
+            popular_items.append((handle, accessed))
+        if len(popular_items) > 4:
+            #print('Found enough popular items!')
+            break
 
 # build a new tree for uploading
 root = ET.Element("document")
@@ -53,10 +55,9 @@ refset.attrib["n"]  = "community-browser"
 
 
 # add elements
-for uid, accesses in popular_items:
+for handle, accesses in popular_items:
 	mets_str = "/metadata/handle/{}/mets.xml"
 	ref = ET.SubElement(refset, "reference")
-	handle = uid_to_handle(uid)
 	ref.attrib["repositoryID"] = handle.split("/")[0]
 	ref.attrib["url"] = mets_str.format(handle)
 	ref.attrib["type"] = "Dspace Item"
